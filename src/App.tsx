88dbbfd196c99de8516f13f60d3559e2c863dddb
amasztak3281 <itzalqxander@gmail.com>
2023-01-07 02:03:38 -0600

    getStartedButton started

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Landing from './components/front-page/Landing';
 import RecipeFinder from './components/food-finder-page/RecipeFinder';
+import GetStartedButton from './global/GetStartedButton';
 //test
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/recipefinder" element={<RecipeFinder />} />
       </Routes>
+      <GetStartedButton></GetStartedButton>
     </div>
   );
 }
--- src/global/GetStartedButton.tsx
+++ src/global/GetStartedButton.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 
 const GetStartedButton: React.FC<{}> = () => {
-    return <div>
-
+    return <div className="flex justify-center">
+        <div>
+        <button className="bg-[#0ACF83] px-16 py-1.5 rounded-full text-white mx-auto">
+        Get started
+        </button>
+        </div>
     </div>;
 };
 
