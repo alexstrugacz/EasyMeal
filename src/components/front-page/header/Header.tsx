c39a407ae2ba1584eeb58675f76e9b59e3ccb043
Alexander Masztak <itzalqxander@gmail.com>
2023-01-07 04:43:51 -0600

    changes

--- src/components/front-page/header/Header.tsx
+++ src/components/front-page/header/Header.tsx
@@ -6,14 +6,16 @@ const Header: React.FC<{}> = () => {
     <div>
       <section
         style={{
-          //backgroundImage:
+          backgroundImage: `url(https://i.ibb.co/P53mdMn/Header-Background-Image.png)`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
         }}
-        className="py-12 px-4"
+        className="w-full h-[754px]"
       >
-        <h1 className="font-Raleway text-5xl font-bold text-center text-[#0ACF83] leading-none tracking-tight order-2 w-381px h-92px">
+        <h1 className="font-Raleway text-7xl font-bold text-center text-[#0ACF83] leading-none tracking-tight order-2 w-381px h-92px pt-[200px]">
           EasyMeal
         </h1>
-        <p className="font-Raleway text-x1 leading-normal text-center order-3 w-987px h-112px">
+        <p className="font-Raleway text-2xl text-white leading-normal text-center order-3 w-987px h-112px pt-4 pb-6">
           Instantly find high-protein meals
           <br />
           using what’s already in your kitchen
