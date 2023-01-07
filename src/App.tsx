1e43c046ab7cd48f71ab6796bf3e2bcbac94fd63
Alex Strugacz <alexstrugacz@icloud.com>
2023-01-07 01:15:59 -0600

    fix runtime error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
 import Landing from './components/front-page/Landing';
 import RecipeFinder from './components/food-finder-page/RecipeFinder';
 
