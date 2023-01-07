8a249dda11b9a6640ebcb5738c8c8556b8a2b3eb
Alex Strugacz <alexstrugacz@icloud.com>
2023-01-07 02:07:18 -0600

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Landing from './components/front-page/Landing';
 import RecipeFinder from './components/food-finder-page/RecipeFinder';
 //test
+//test2
 
 function App() {
   return (
--- src/components/front-page/body/NutritionMadeEasy.tsx
+++ src/components/front-page/body/NutritionMadeEasy.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const NutritionMadeEasy: React.FC<{}> = () => {
     return <div className="w-full h-[754px] bg-slate-200">
-        <div>
-            <h1 className="">Nutrition Made Easy</h1>
-            <h2>Find super nutritious meals in just 3 simple steps</h2>
+        <div className="text-center">
+            <h1 className="font-bold font-Inter text-4xl text-[#252B42]">Nutrition Made Easy</h1>
+            <h2 className="font-regular font-Inter text-2xl text-[#374754]">Find super nutritious meals in just 3 simple steps</h2>
         </div>
     </div>;
 };
