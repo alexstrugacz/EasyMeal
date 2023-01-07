c3f31c6adb0fdc8510b891a880c47235f345d947
Alex Strugacz <alexstrugacz@icloud.com>
2023-01-07 02:00:16 -0600

    added regular + bold Inter, Raleway font

--- src/components/front-page/body/NutritionMadeEasy.tsx
+++ src/components/front-page/body/NutritionMadeEasy.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 
 const NutritionMadeEasy: React.FC<{}> = () => {
-    return <div>
-
+    return <div className="w-full h-[754px] bg-slate-200">
+        <div>
+            <h1 className="">Nutrition Made Easy</h1>
+            <h2>Find super nutritious meals in just 3 simple steps</h2>
+        </div>
     </div>;
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+        Raleway: ["Raleway", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
