d87e29f259a0d6c3abe91cae72abeefb5d5bc71d
Alexander Masztak <itzalqxander@gmail.com>
2023-01-07 10:35:01 -0600

    .

--- src/components/front-page/Landing.tsx
+++ src/components/front-page/Landing.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './header/Header';
 import Body from "./body/NutritionMadeEasy"
+import Body2 from "./body/Science"
 import Footer from "./footer/GetStartedFooter"
 
 const Landing: React.FC<{}> = () => {
     return <div>
         <Header />
         <Body />
+        <Body2 />
         <Footer />
     </div>;
 };
--- src/components/front-page/body/NutritionMadeEasy.tsx
+++ src/components/front-page/body/NutritionMadeEasy.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NutritionMadeEasy: React.FC<{}> = () => {
-    return <div className="w-full h-[754px] bg-white">
+    return <div className="w-full h-[454px] bg-white">
         <div className="text-center">
             <h1 className="font-bold font-Inter text-4xl text-[#252B42]">Nutrition Made Easy</h1>
             <h2 className="font-regular font-Inter text-2xl mt-4 text-[#374754]">Find super nutritious meals in just <br></br>3 simple steps</h2>
