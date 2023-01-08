514e31a676084b13150a430756f642e3e2629e3b
nexus1010 <bot.developer3@gmail.com>
2023-01-07 22:28:41 -0600

    optimized tabbar + section

---
+++ src/components/food-finder-page/ResultItem/ResultItem.tsx
@@ -0,0 +1,35 @@
+import * as React from "react"
+import { IRecipe } from "../../../types/IRecipe";
+const ResultItem: React.FC<{
+    recipe: IRecipe
+}> = (props) => {
+
+    const [displayedScore, setDisplayedScore] = React.useState<string | undefined>();
+
+    React.useEffect(() => {
+        if (props.recipe.score) {
+            setDisplayedScore(props.recipe.score.toFixed(1));
+        }
+    }, [props.recipe.score]);
+
+    return (
+
+        <div className='hover:scale-[1.02] transition-all'>
+            <div className='flex flex-col justify-between bg-[#F0F0F0] shadow-md rounded-md p-3 h-[500px]'>
+                <img className={"bg-zinc-400 object-cover rounded-2xl w-full h-64"} src={props.recipe.recipe.imageUrl} />
+                <p className={"font-Inter text-[#E0E0E] text-left font-medium font-bold text-xl mt-4"}>{props.recipe.recipe.header}</p>
+                <div className='flex text-[#0E0E0E]'>
+                    <p className={"font-Inter text-left first-letter:text-md mt-1"}>{`${props.recipe.recipe.calories} calories`}</p>
+                </div>
+                <p className={`font-regular pt-1 font-Inter ${props.recipe.pctAvailable > 0 ? "text-[#25B87F]" : "text-zinc-400"}`}>{`${props.recipe.ingredientCount}/${props.recipe.totalIngredients} ingredients ready`}</p>
+                <div className={"bg-[#25B87F] rounded-lg p-2 py-2 mt-3"}>
+                    <p className={"text-zinc-100 font-Inter text-md"}>
+                        EasyMeal Health Score
+                    </p>
+                    <p className={"text-white font-Inter font-bold text-lg"}>{displayedScore}/10.0</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default ResultItem
--- src/components/food-finder-page/ResultSection/ResultsSection.tsx
+++ src/components/food-finder-page/ResultSection/ResultsSection.tsx
@@ -1,37 +1,54 @@
 import React from 'react';
 import { IRecipe } from '../../../types/IRecipe';
+import ResultItem from '../ResultItem/ResultItem';
 
 const ResultsSection: React.FC<{
     recipes: IRecipe[] | undefined
 }> = (props) => {
+
+    const [displayedRecipes, setDisplayedRecipes] = React.useState<IRecipe[]>([]);
+    const [currentIndex, setCurrentIndex] = React.useState<number>(0);
+
+    const handleLoadMore = () => {
+        const totalLength = props.recipes?.length || 0;
+
+        if (currentIndex + 10 < totalLength) {
+            setDisplayedRecipes(props.recipes?.slice(0, currentIndex + 10) || []);
+            setCurrentIndex(currentIndex + 10);
+        } else {
+            setDisplayedRecipes(props.recipes || []);
+            setCurrentIndex(totalLength);
+        }
+    }
+
+    React.useEffect(() => {
+        handleLoadMore();
+    }, [props.recipes])
+
     if (props.recipes) {
-        return <div className={"flex-1"}>
-            <div className="m-8 grid grid-cols-1 md:grid-cols-2 gap-4 wrap">
-                {props.recipes.map((recipe, index) => {
+        return <div className={"flex-1 h-full overflow-auto"}>
+            <div className="m-8 grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 3xl:grid-cols-4 gap-4 wrap">
+                {displayedRecipes.map((recipe, index) => {
                     return (
-                        <div className='hover:scale-[1.02] transition-all'>
-                            <div className='bg-[#F0F0F0] shadow-md shadow-neutral-500 rounded-md h-[500px] p-3'>
-                                <img className={"bg-gradient-to-r from-cyan-500 to-blue-500 object-cover rounded-2xl w-full h-64"} src={recipe.recipe.imageUrl} />
-                                <p className={"font-Inter text-[#E0E0E] text-left font-medium font-bold text-xl mt-4"}>{recipe.recipe.header}</p>
-                                <div className='flex text-[#0E0E0E]'>
-                                    <p className={"font-Inter text-left first-letter:text-md mt-1"}>{`${recipe.recipe.calories} calories -  ${Math.round(100 * recipe.recipe.weightLossScore) / 100} Score`}</p>
-                                </div>
-                                <p className='font-regular pt-1 font-Inter text-[#25B87F]'>{`${recipe.ingredientCount} / ${recipe.totalIngredients} ingredients ready`}</p>
-                                <div className='pb-4 flex flex-wrap gap-2 mt-4 text-md font-Inter'>
-                                    {recipe.recipe.ingredients[1] + recipe.recipe.ingredients[2]}
-                                </div>
-                            </div>
-
-
-                        </div>
+                        <ResultItem
+                            key={`recipe-${index}`}
+                            recipe={recipe}
+                        />
                     )
-
                 })}
+                <div className={"bg-blue-300 h-10"}>
+                    <p>PAGINATION DIV</p>
+
+                </div>
+
+
+
+
             </div>;
         </div>
     } else {
-        return <div className={"flex-1"}>
-            <p></p>
+        return <div className={"flex flex-1 justify-center items-center h-screen p-10"}>
+            <p className={"font-Inter text-2xl font-semibold text-[#f7aa47]"}>Add some filters to get meals!</p>
         </div>
     }
 };
