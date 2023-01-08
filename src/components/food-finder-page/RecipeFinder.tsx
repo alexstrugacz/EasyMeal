839fe32bfc9e66a2b279e1ae90671c0e3fdafce1
Alexander Masztak <itzalqxander@gmail.com>
2023-01-07 22:15:00 -0600

    .

--- src/components/food-finder-page/RecipeFinder.tsx
+++ src/components/food-finder-page/RecipeFinder.tsx
@@ -5,7 +5,7 @@ import TabBar from "./TabBar/TabBar";
 const RecipeFinder: React.FC<{}> = () => {
     return <div>
         <TabBar />
-        <ResultsSection show={true} />
+        <ResultsSection show={false} />
     </div>;
 };
 
