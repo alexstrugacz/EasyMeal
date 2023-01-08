bdfb09c4acfa82fe7c3b80856933b1e92c456e43
Alex Strugacz <90818590+Nectar0@users.noreply.github.com>
2023-01-07 19:55:58 -0600

    added recipe display functionality

--- src/components/food-finder-page/ResultSection/ResultsSection.tsx
+++ src/components/food-finder-page/ResultSection/ResultsSection.tsx
@@ -6,13 +6,29 @@ const ResultsSection: React.FC<{
 }> = (props) => {
     if (props.recipes) {
         return <div className={"flex-1"}>
-            {props.recipes.map((recipe, index) => {
-                return (
-                    <p key={`recipe-${index}`}>{recipe.recipe.header}</p>
-                )
+            <div className="m-8 grid grid-cols-1 md:grid-cols-2 gap-4 wrap">
+                {props.recipes.map((recipe, index) => {
+                    return (
+                        <div className='hover:scale-[1.02] transition-all'>
+                            <div className='bg-[#F0F0F0] shadow-md shadow-neutral-500 rounded-md h-[500px] p-3'>
+                                <img className={"bg-gradient-to-r from-cyan-500 to-blue-500 object-cover rounded-2xl w-full h-64"} src={recipe.recipe.imageUrl} />
+                                <p className={"font-Inter text-[#E0E0E] text-left font-medium font-bold text-xl mt-4"}>{recipe.recipe.header}</p>
+                                <div className='flex text-[#0E0E0E]'>
+                                    <p className={"font-Inter text-left first-letter:text-md mt-1"}>{`${recipe.recipe.calories} calories -  ${Math.round(100 * recipe.recipe.weightLossScore) / 100} Score`}</p>
+                                </div>
+                                <p className='font-regular pt-1 font-Inter text-[#25B87F]'>{`${recipe.ingredientCount} / ${recipe.totalIngredients} ingredients ready`}</p>
+                                <div className='pb-4 flex flex-wrap gap-2 mt-4 text-md font-Inter'>
+                                    {recipe.recipe.ingredients[1] + recipe.recipe.ingredients[2]}
+                                </div>
+                            </div>
 
-            })}
-        </div>;
+
+                        </div>
+                    )
+
+                })}
+            </div>;
+        </div>
     } else {
         return <div className={"flex-1"}>
             <p></p>
--- src/hooks/use-recipe-finder-hook.ts
+++ src/hooks/use-recipe-finder-hook.ts
@@ -1,45 +1,40 @@
-import {
-    useState
-} from "react";
+import { useState } from "react";
 import { IRecipe } from "../types/IRecipe";
 
 export const useRecipeFinderHook = () => {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState<string | undefined>();
-    const [results, setResults] = useState<IRecipe[] | undefined>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | undefined>();
+  const [results, setResults] = useState<IRecipe[] | undefined>();
 
-    const handleLoadRecipes = async (
-        ingredients: string[],
-        nutritionGoal: string,
-        minCalories: number | undefined,
-        maxCalories: number | undefined,
-    ) => {
-
-
-        let url = `https://easymeal.herokuapp.com/recipes?ingredients=${ingredients.join(",")}&fitnessGoal=${nutritionGoal}`
-        if (minCalories) {
-            url += `&minCalories=${minCalories}`
-        }
-
-        if (maxCalories) {
-            url += `&maxCalories=${maxCalories}`
-        }
-
-        console.log(url);
-        const data = await (
-            await fetch(url)
-        ).json()
-
-        const newRecipes = data.results as IRecipe[];
-        console.log("new recipes", newRecipes);
-
-        setResults(newRecipes);
+  const handleLoadRecipes = async (
+    ingredients: string[],
+    nutritionGoal: string,
+    minCalories: number | undefined,
+    maxCalories: number | undefined
+  ) => {
+    let url = `https://easymeal.herokuapp.com/recipes?ingredients=${ingredients.join(
+      ","
+    )}&fitnessGoal=${nutritionGoal}`;
+    if (minCalories) {
+      url += `&minCalories=${minCalories}`;
     }
 
-    return {
-        error,
-        results,
-        handleLoadRecipes
+    if (maxCalories) {
+      url += `&maxCalories=${maxCalories}`;
     }
 
-}
+    console.log(url);
+    const data = await (await fetch(url)).json();
+
+    const newRecipes = data.results as IRecipe[];
+    console.log("new recipes", newRecipes);
+
+    setResults(newRecipes);
+  };
+
+  return {
+    error,
+    results,
+    handleLoadRecipes,
+  };
+};
